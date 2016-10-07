a4a0c7bd66d8804376c0c449f6466dbfe4688e35
Lucas <lffsantos@gmail.com>
2016-10-06 23:10:36 -0300

    add test

--- service/__init__.py
+++ service/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask.ext.cors import CORS
 from flask_restful import Api
 
 app = Flask(__name__)
@@ -8,5 +7,10 @@ api_v1 = Api(app, prefix='/api/v1')
 # then import views which use the blueprint
 from service.events import views
 
-# now register the blueprint with the app
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
---
+++ test/test_events_view.py
@@ -0,0 +1,23 @@
+import json
+import re
+from flask import url_for
+import pytest
+import service
+
+
+@pytest.fixture
+def app():
+    service.app.config['SERVER_NAME'] = "TEST_SERVER"
+    service.app.config['TESTING'] = True
+    return service.app
+
+
+@pytest.mark.parametrize('test_case', [
+    {'groups_meetup': json.dumps(['pythonireland'])},
+    {'groups_meetup': json.dumps(['pythonireland', 'django-dublin'])},
+])
+def test_get_event_cards(client, test_case):
+    response = client.get(url_for('eventsmeetup'), query_string=test_case)
+    for group in json.loads(test_case['groups_meetup']):
+        url = "http://www.meetup.com/{0}/".format(group)
+        assert re.search(url, response.json['html'])
